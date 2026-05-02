df99ddd66dcafc88e3ec85549f3b7c0ff4396e2c
Dpto. Madera <dpto.madera@gmail.com>
2026-05-02 07:53:08 +0100

    Migración: js/ocr-external-config.js

---
+++ js/ocr-external-config.js
@@ -0,0 +1,8 @@
+/**
+ * Configuracion de OCR externo
+ * Rellena estos valores segun tu proveedor.
+ */
+
+export const EXTERNAL_OCR_URL = 'https://gmedcas-merterbakdeepseekocr.hf.space/ocr';
+export const EXTERNAL_OCR_API_KEY = '';
+export const EXTERNAL_OCR_TIMEOUT_MS = 45000;
